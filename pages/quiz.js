d51134868352302d2a746a80cc4677be426b290b
Vitor Quadros <viitorquadros@hotmail.com>
2021-01-27 19:28:58 -0300

    feat: quiz page structure

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,7 +1,41 @@
 import React from "react";
+import db from "../db.json";
 
-const Quiz = () => {
-  return <div></div>;
+import Widget from "../src/components/Widget.js";
+import GitHubCorner from "../src/components/GitHubCorner.js";
+import QuizBackground from "../src/components/QuizBackground.js";
+import QuizContainer from "../src/components/QuizContainer.js";
+import Button from "../src/components/Button.js";
+
+const QuizPage = () => {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h3>Pergunta 1 de {` ${db.questions.length}`}</h3>
+          </Widget.Header>
+          <img
+            alt="Descrição"
+            style={{
+              width: "100%",
+              height: "150px",
+              objectFit: "cover",
+            }}
+            src="https://media.giphy.com/media/xUOxf3yDKCuwpOlT3i/giphy.gif"
+          />
+
+          <Widget.Content>
+            <h2>Titulo</h2>
+            <p>Descrição</p>
+
+            <Button>Confirmar</Button>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+      <GitHubCorner projectUrl="" />
+    </QuizBackground>
+  );
 };
 
-export default Quiz;
+export default QuizPage;
