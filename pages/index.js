1aa32e1773df69296a9e386aadd9fd7760852072
Vitor Quadros <viitorquadros@hotmail.com>
2021-01-27 19:28:29 -0300

    refactor: file system

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import db from "../db.json";
+
 import { useRouter } from "next/router";
 import Widget from "../src/components/Widget.js";
 import Footer from "../src/components/Footer.js";
 import GitHubCorner from "../src/components/GitHubCorner.js";
 import QuizBackground from "../src/components/QuizBackground.js";
+import QuizContainer from "../src/components/QuizContainer.js";
 import Input from "../src/components/Input.js";
 import Button from "../src/components/Button.js";
 
@@ -15,62 +17,52 @@ import Button from "../src/components/Button.js";
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState("");
-
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>Teste</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                router.push(`/quiz?name=${name}`);
-              }}
-            >
-              <Input
-                name="nomeDoUsuario"
-                placeholder="Seu nome"
-                type="text"
-                onChange={({ target }) => setName(target.value)}
-              />
-              <Button type="submit" disabled={name.length === 0}>
-                Jogar
-              </Button>
-            </form>
-          </Widget.Content>
-        </Widget>
+    <>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>Teste</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  router.push(`/quiz?name=${name}`);
+                }}
+              >
+                <Input
+                  name="nomeDoUsuario"
+                  placeholder="Seu nome"
+                  type="text"
+                  onChange={({ target }) => setName(target.value)}
+                />
+                <Button type="submit" disabled={name.length === 0}>
+                  Jogar
+                </Button>
+              </form>
+            </Widget.Content>
+          </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Teste 2</h1>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Aspernatur ea earum modi veniam vero minus accusantium quas
-              dolores molestiae repudiandae velit et maxime quam, reprehenderit
-              possimus provident id nesciunt sit.
-            </p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="" />
-    </QuizBackground>
+          <Widget>
+            <Widget.Content>
+              <h1>Teste 2</h1>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aspernatur ea earum modi veniam vero minus accusantium quas
+                dolores molestiae repudiandae velit et maxime quam,
+                reprehenderit possimus provident id nesciunt sit.
+              </p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="" />
+      </QuizBackground>
+    </>
   );
 }
